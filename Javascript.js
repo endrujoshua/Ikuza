91c17031dfb01cd6617546199222cc82400d52f8
chiaki <chiaki.waifu@gmail.com>
2023-02-23 17:35:21 +0800

    aaa

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
 function changeBackground() {
     var bg = ["main1.jpg", "main2.jpg", "main3.jpg", "main4.jpg"];
-    var randombg = bg[Math.floor(Math.random() * colors.length)];
+    var randombg = bg[Math.floor(Math.random() * bg.length)];
     document.body.background = "Images/"+randombg;}
