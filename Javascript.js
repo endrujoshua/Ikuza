d25d37f66701a2754cc2e625c63bd845eb4e04c9
chiaki <chiaki.waifu@gmail.com>
2023-02-23 20:13:51 +0800

    Preload

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,6 @@
 const images = [
     "Images/main1.jpg","Images/main2.jpg", "Images/main3.jpg", "Images/main4.jpg"
   ];
-for(x=0; x++; x<images.length){
-    preloadImage(images[i]);
-}
-function preloadImage(url)
-{
-    var img=new Image();
-    img.src=url;
-}
 
   var i = 0;
   setInterval(function() {
