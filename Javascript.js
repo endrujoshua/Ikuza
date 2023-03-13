d6dc1e3197b28d0d7711e0e16c68dba46ae0057a
BlueMinion <blueminion.98@gmail.com>
2023-03-13 14:24:25 +0700

    update

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,11 @@
 /* Navbar */
 /* Collapse Button */
-var collapse_content = document.getElementById("collapse-container-nav-secondary");
-var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
-var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
-var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
-var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
 function collapse_btn(){
+  var collapse_content = document.getElementById("collapse-container-nav-secondary");
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
   if (collapse_content.style.display === "none"){
     collapse_content.style.display = "block";
   } else {
@@ -15,10 +15,21 @@ function collapse_btn(){
     dropdown_content_btn2.style.display = "none";
     dropdown_content_btn3.style.display = "none";
   }
+  var title_textbox = document.getElementsByClassName("title-textbox");
+  var description_textbox = document.getElementsByClassName("description-textbox");
+
+  if (window.innerWidth < 600 && collapse_content.style.display === "block"){
+    title_textbox.style.display = "none";
+    description_textbox.display = "none";
+  }
 }
 
 /* Dropdown Button */
 function dropdown_btn(){
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
   if (dropdown_content.style.display === "none"){
     dropdown_content.style.display = "block";
     dropdown_content_btn1.style.display = "none";
@@ -31,6 +42,10 @@ function dropdown_btn(){
 
 /* Sosmed Button 1 */
 function sosmed1_btn(){
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
   if (dropdown_content_btn1.style.display === "none"){
     dropdown_content.style.display = "none";
     dropdown_content_btn1.style.display = "block";
@@ -43,6 +58,10 @@ function sosmed1_btn(){
 
 /* Sosmed Button 2 */
 function sosmed2_btn(){
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
   if (dropdown_content_btn2.style.display === "none"){
     dropdown_content.style.display = "none";
     dropdown_content_btn1.style.display = "none";
@@ -55,6 +74,10 @@ function sosmed2_btn(){
 
 /* Sosmed Button 3 */
 function sosmed3_btn(){
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav");
   if (dropdown_content_btn3.style.display === "none"){
     dropdown_content.style.display = "none";
     dropdown_content_btn1.style.display = "none";
@@ -65,6 +88,7 @@ function sosmed3_btn(){
   }
 }
 /* Navbar End */
+
 
 const images = [
     "Images/main1.jpg","Images/main2.jpg", "Images/main3.jpg", "Images/main4.jpg"
