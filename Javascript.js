355c7785c8011c7a2973e08f71431a19d6165b5c
Elulu Virnashya <chiaki.waifu@gmail.com>
2023-03-13 19:11:38 +0800

    asdf

--- Javascript.js
+++ Javascript.js
@@ -118,20 +118,34 @@ const images = [
   }, 4000);*/
   var Y = document.documentElement.clientHeight 
 function ChangeBackground() {
+  
+  let i =0;
   var body = document.body;
   var currentPosition = window.pageYOffset;
   var maxHeight = screen.availHeight;
+  var meter = document.getElementsByClassName("meter");
   console.log(maxHeight);
-  if (currentPosition < maxHeight) {
+  if (currentPosition < maxHeight*0.75) {
     body.style.backgroundImage = "url('Images/main1.jpg')";
-  } else if (currentPosition >= maxHeight && currentPosition <= maxHeight*2) {
+    i = 0;
+  } else if (currentPosition >= maxHeight*0.75 && currentPosition <= maxHeight*1.5) {
     body.style.backgroundImage = "url('Images/main2.jpg')";
-  } else if (currentPosition >= maxHeight*2 && currentPosition <= maxHeight*3) {
+    i=1;
+  } else if (currentPosition >= maxHeight*1.5 && currentPosition <= maxHeight*2.25) {
     body.style.backgroundImage = "url('Images/main3.jpg')";
+    i=2;
   }
   else{
     body.style.backgroundImage = "url('Images/main4.jpg')"
+    i=3;
   }
-
+  for(let x = 0; x <meter.length; x++){
+    if(x==i){
+      meter[x].style.backgroundColor = "rgba(247, 164, 164, 1)"
+    }
+    else{
+      meter[x].style.backgroundColor = "rgba(247, 164, 164, 0.25)"
+    }
+  }
 };
 window.addEventListener("scroll", ChangeBackground , false);
