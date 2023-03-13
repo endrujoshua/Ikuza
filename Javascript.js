dc533ea86c0400b67d8c511d7a4f9a2063d0aaab
Elulu Virnashya <chiaki.waifu@gmail.com>
2023-03-13 14:26:51 +0800

    Advanced Javascript

--- Javascript.js
+++ Javascript.js
@@ -12,64 +12,27 @@ const images = [
   }, 4000);*/
   var Y = document.documentElement.clientHeight 
 function myFunction() {
-  if(this.scrollY <2000){
-  document.body.style.backgroundImage = this.scrollY > 1000
-     ? "url(Images/main2.jpg)"
-     : "url(Images/main1.jpg)";
-  }
-  else{
-    document.body.style.backgroundImage = this.scrollY > 3000
-     ? "url(Images/main4.jpg)"
-     : "url(Images/main3.jpg)";
-  }
+  const scrollPosition = this.scrollY;
+const body = document.body;
+
+const backgroundImage = scrollPosition < 2000
+  ? (scrollPosition > 1000 ? "url(Images/main2.jpg)" : "url(Images/main1.jpg)")
+  : (scrollPosition > 3000 ? "url(Images/main4.jpg)" : "url(Images/main3.jpg)");
+
+body.style.backgroundImage = backgroundImage;
 var meter = document.getElementsByClassName("meter");
 var i = 0;
-  if(this.scrollY<1000){
-    i= Math.floor(this.scrollY/1000);
-        for(let x=0;x<meter.length;x++){
-            if(i== x){
-              meter[x].style.backgroundColor= "rgba(247, 164, 164, 1 )";
-            }
-            else{
-              meter[x].style.backgroundColor= "rgba(247, 164, 164, 0.25 )";
-            }
-        }
+if (this.scrollY < 4000) {
+  const i = Math.floor(this.scrollY / 1000);
+  for (let x = 0; x < meter.length; x++) {
+    meter[x].style.backgroundColor = i === x ? "rgba(247, 164, 164, 1 )" : "rgba(247, 164, 164, 0.25 )";
   }
-  else if (1000<this.scrollY<2000){
-    i= Math.floor(this.scrollY/1000);
-    for(let x=0;x<meter.length;x++){
-      if(i== x){
-        meter[x].style.backgroundColor= "rgba(247, 164, 164, 1 )";
-      }
-      else{
-        meter[x].style.backgroundColor= "rgba(247, 164, 164, 0.25 )";
-      }
+} else {
+  const i = Math.floor(this.scrollY / 1000);
+  for (let x = 0; x < meter.length; x++) {
+    meter[x].style.backgroundColor = i === x ? "rgba(247, 164, 164, 1 )" : "rgba(247, 164, 164, 0.25 )";
   }
 }
-   else if(2000<this.scrollY<3000){
-    i= Math.floor(this.scrollY/1000);
-    for(let x=0;x<meter.length;x++){
-      if(i== x){
-        meter[x].style.backgroundColor= "rgba(247, 164, 164, 1 )";
-      }
-      else{
-        meter[x].style.backgroundColor= "rgba(247, 164, 164, 0.25 )";
-      }
-  }
-   }
-    else{
-
-      i= Math.floor(this.scrollY/1000);
-      for(let x=0;x<meter.length;x++){
-        if(i== x){
-          meter[x].style.backgroundColor= "rgba(247, 164, 164, 1 )";
-        }
-        else{
-          meter[x].style.backgroundColor= "rgba(247, 164, 164, 0.25 )";
-        }
-    } 
-
-  }
 };
 
 window.addEventListener("scroll", myFunction , false);
