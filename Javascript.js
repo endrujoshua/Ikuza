c0f65b181b5d0cbbdc4e17a4ce5f66953051a1b7
BlueMinion <blueminion.98@gmail.com>
2023-03-16 02:22:36 +0700

    updates

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
-/* Navbar */
+/* Homepage Navbar */
 /* Collapse Button */
 function collapse_btn(){
   var collapse_content = document.getElementById("collapse-container-nav-secondary");
@@ -174,3 +174,23 @@ function ChangeBackground() {
   }
 };
 window.addEventListener("scroll", ChangeBackground , false);
+
+
+/* Gallery Navbar */
+/* Collapse Button */
+function collapse_btn_gallery(){
+  var collapse_content = document.getElementById("collapse-container-nav-secondary");
+  var dropdown_content = document.getElementById("dropdown-content-nav-secondary-gallery");
+  var dropdown_content_btn1 = document.getElementById("p1-dropdown-content-sosmed-nav-gallery");
+  var dropdown_content_btn2 = document.getElementById("p2-dropdown-content-sosmed-nav-gallery");
+  var dropdown_content_btn3 = document.getElementById("p3-dropdown-content-sosmed-nav-gallery");
+  if (collapse_content.style.display === "none"){
+    collapse_content.style.display = "block";
+  } else {
+    collapse_content.style.display = "none";
+    dropdown_content.style.display = "none";
+    dropdown_content_btn1.style.display = "none";
+    dropdown_content_btn2.style.display = "none";
+    dropdown_content_btn3.style.display = "none";
+  }
+}
