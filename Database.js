3b4d4dde74adc8b6ec0978e10cb538c05bcd3589
Elulu Virnashya <chiaki.waifu@gmail.com>
2023-03-21 17:51:50 +0800

    asdf

---
+++ Database.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF0P_LS6mmygNUu4OF2cze2cAAg1sl01k",
+  authDomain: "ikuza-f9f15.firebaseapp.com",
+  projectId: "ikuza-f9f15",
+  storageBucket: "ikuza-f9f15.appspot.com",
+  messagingSenderId: "332743558243",
+  appId: "1:332743558243:web:be91190d94c4973efda456"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
