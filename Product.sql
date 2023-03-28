7b6969b3fa9249f028330c450311c29e475974f0
Elulu Virnashya <chiaki.waifu@gmail.com>
2023-03-28 15:42:56 +0800

    sql

--- Product.sql
+++ Product.sql
@@ -1,19 +1,28 @@
-CREATE DATABASE ProductDB;
+CREATE DATABASE Product_db
+CREATE TABLE category(
+    CategoryID int NOT NULL AUTO_INCREMENT,
+    CategoryName varchar(255)
+)
 CREATE TABLE product_detail(
     ProductID int NOT NULL AUTO_INCREMENT,
     ProductImage varchar(255),
-    ProductCategory varchar(255),
+    CategoryID int,
     ProductName varchar(255),
     ProductDesc varchar(255),
     ProductPrice varchar(255),
-    ProductColor1 varchar(255),
-    ProductColor2 varchar(255),
-    ProductColor3 varchar(255),
+    PRIMARY KEY (ProductID),
+    FOREIGN KEY (CategoryID) REFERENCES category(CategoryID)
+)
+CREATE TABLE colour(
+    ColourID int NOT NULL AUTO_INCREMENT,
+    ColourHash varchar(255),
+    ColourName varchar(255)
 )
 
-UPDATE ProductID
-SET ProductImage = 'Images/product/Bed_1.jpg'
-SET ProductName = 'Chikaku'
-SET ProductDesc = "It's firm yet soft."
-SET ProductPrice = '¥40000'
-
+CREATE TABLE colour_product{
+    ProductID int NOT NULL,
+    ColourID int NOT NULL,
+    FOREIGN KEY (ProductID) REFERENCES product_detail(ProductID),
+    FOREIGN KEY (ColourID) REFERENCES colour(ColourID)
+}
+Select * from *
